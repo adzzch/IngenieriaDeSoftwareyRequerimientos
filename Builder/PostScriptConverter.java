c55861748c79fdcec383478fb48de92d4cc6550e
Alejandro Zamora <daniel.zamorach@campusucc.edu.co>
2025-08-06 00:32:31 +0000

    ola ola ola

---
+++ Builder/PostScriptConverter.java
@@ -0,0 +1,14 @@
+public class PostScriptConverter extends Converter {
+    
+    public void makeLine() {
+        System.out.println("Linea con PDF");
+    }
+
+    public void makeParagraph() {
+        System.out.println("Párrafo con PDF");
+    }
+
+    public void makeTable() {
+        System.out.println("Tabla con PDF");
+    }
+}
